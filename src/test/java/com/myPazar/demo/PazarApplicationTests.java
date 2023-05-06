09b02c4b78f714ceffab7eb6d273e306bd746a86
ibrahimAlroubh <ibrahrpbh@gmail.com>
2023-05-06 07:44:48 -0700

    update

---
+++ src/test/java/com/myPazar/demo/PazarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myPazar.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PazarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
