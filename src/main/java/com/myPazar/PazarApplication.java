5102e638280d8e0a9192ea93aa693dd9c78328c9
ibrahimAlroubh <ibrahrpbh@gmail.com>
2023-05-11 18:03:56 -0700

    update

---
+++ src/main/java/com/myPazar/PazarApplication.java
@@ -0,0 +1,14 @@
+package com.myPazar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+
+@SpringBootApplication
+//@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+public class PazarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PazarApplication.class, args);
+	}
+}
---
+++ src/main/java/com/myPazar/model/Customer.java
@@ -0,0 +1,7 @@
+package com.myPazar.model;
+
+import lombok.Data;
+
+@Data
+public class Customer {
+}
